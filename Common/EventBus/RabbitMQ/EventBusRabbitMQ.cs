d60310a688a7e3a27dd642f15ad54e3644b49cd2
lromero <lromero@hexacta.com>
2018-06-26 13:24:50 -0300

    added provider api

--- Common/EventBus/RabbitMQ/EventBusRabbitMQ.cs
+++ Common/EventBus/RabbitMQ/EventBusRabbitMQ.cs
@@ -20,13 +20,13 @@ namespace EventBusRabbitMQ
 {
     public class EventBusRabbitMQ : IEventBus, IDisposable
     {
-        const string BROKER_NAME = "eshop_event_bus";
+        const string BROKER_NAME = "mervi_event_bus";
 
         private readonly IRabbitMQPersistentConnection _persistentConnection;
         private readonly ILogger<EventBusRabbitMQ> _logger;
         private readonly IEventBusSubscriptionsManager _subsManager;
         private readonly ILifetimeScope _autofac;
-        private readonly string AUTOFAC_SCOPE_NAME = "eshop_event_bus";
+        private readonly string AUTOFAC_SCOPE_NAME = "mervi_event_bus";
         private readonly int _retryCount;
 
         private IModel _consumerChannel;
